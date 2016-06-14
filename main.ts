76dafe677ce79b9f56ea8c9c9e7afbc7de422aa2
Mihail <mihail.davitkovski@endava.com>
2016-06-14 16:20:29 +0200

    Adding main.ts file

---
+++ main.ts
@@ -0,0 +1,15 @@
+
+/// <reference path="./node_modules/angular2/typings/browser.d.ts" />
+
+import {bootstrap} from "angular2/platform/browser";
+
+import {RedditComponent} from "./app/articles/reddit.component"; 
+
+import {InventoryComponent} from "./app/inventory/inventory.component"; 
+
+import {DemoFormSkuComponent} from "./app/forms/demo-form-sku.component"; 
+
+import {DemoFormSkuBuilderComponent} from "./app/forms/demo-form-sku-with-builder.component"; 
+
+bootstrap(InventoryComponent);
+

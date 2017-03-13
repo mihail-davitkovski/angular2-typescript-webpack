8c655a40c9147fd76bcc7c840e393f1f4c5c4ef0
Mihail <mihail.davitkovski@endava.com>
2017-03-13 17:32:25 +0100

    Adding routes

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
 
+import {
+    RouterModule,
+    Routes
+} from '@angular/router';
+
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
-import {InventoryAppModule} from "./app/inventory/inventory.module"; 
+import { InventoryAppModule } from "./app/inventory/inventory.module";
+
+const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: InventoryAppModule }
+];
 
 platformBrowserDynamic().bootstrapModule(InventoryAppModule);
